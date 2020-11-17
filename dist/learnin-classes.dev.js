07f53cc9c51c56d0eba72e6174212fef76a6b8ab
tigthor <tigthor1337@gmail.com>
2020-11-18 00:16:55 +0200

    initial commit

---
+++ dist/learnin-classes.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var group = function group(id, priority, day) {
+  _classCallCheck(this, group);
+
+  this.id = id, this.priority = priority, this.day = day;
+};
+
+console.log(group.name);
